95973de4914475773bd904e45f66b382ea1846c7
cynzenith~ <cynzenith@gmail.com>
2024-07-09 04:23:56 +0900

    second commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news = []
 const getLatestNews = async () => {
         // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`; 
         // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-        const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`); 
+        const url = new URL(`https://inspiring-custard-4a08a3.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`); 
         console.log(url);
         const response = await fetch(url)
         const data = await response.json();
