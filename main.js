f36094839d7bf727716aed5d48f1bc5ed89b8eae
cynzenith~ <cynzenith@gmail.com>
2024-07-12 04:28:29 +0900

    Part7-ex

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const API_KEY = `be38e0d410064dd3b43d39e58e44aeb4`
-let newsList = []
+let news = []
 const getLatestNews = async () => {
         // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`; 
         // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-        const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+        const url = new URL(`https://inspiring-custard-4a08a3.netlify.app/top-headlines`); 
         console.log(url);
         const response = await fetch(url)
         const data = await response.json();
