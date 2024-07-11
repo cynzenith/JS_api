c7b48c9f15561b082fad8522b5e233ad4a7b2280
cynzenith~ <cynzenith@gmail.com>
2024-07-12 04:22:42 +0900

    Part7-ex

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const API_KEY = `be38e0d410064dd3b43d39e58e44aeb4`
-let news = []
+let newsList = []
 const getLatestNews = async () => {
         // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`; 
         // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-        const url = new URL(`https://inspiring-custard-4a08a3.netlify.app/top-headlines`); 
+        const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
         console.log(url);
         const response = await fetch(url)
         const data = await response.json();
@@ -16,11 +16,11 @@ const render=()=>{
     const maxLength = 200;
     const newsHTML = newsList.map(news=>
         `<div class="row news">
-            <div class="col-lg-4">  
+            <div class="col-lg-4">          
                 <img class="news-img-size"
     src="${news.urlToImage || "https://t3.ftcdn.net/jpg/05/79/68/24/360_F_579682479_j4jRfx0nl3C8vMrTYVapFnGP8EgNHgfk.jpg"}"
     onerror="this.onerror=null;this.src='https://t3.ftcdn.net/jpg/05/79/68/24/360_F_579682479_j4jRfx0nl3C8vMrTYVapFnGP8EgNHgfk.jpg';" />
-            </div>        
+            </div>
             <div class="col-lg-8">        
                 <h2>${news.title}</h2>
                 <p>${
@@ -40,3 +40,14 @@ getLatestNews()
 for (let i = 0; i < 5; i++) {
     console.log("after", i);
 }
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+        inputArea.style.display = "none";
+    } else {
+        inputArea.style.display = "inline";
+    }
+};
+
+
